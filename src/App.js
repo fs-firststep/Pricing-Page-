75a23e3003bb38e7c9905f9cf5f8d90756a922a4
Vijay Kumar <vjkr1190@gmail.com>
2024-10-03 21:06:58 +0530

    Practice attempt 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import PricingCard from './components/PricingCard'
 
 function App() {
+
+  const [selectMonthly, setSelectMonthly] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+
+        <header>
+          <h1 className='header-topic'>Our Pricing Plan</h1>
+          <div className='header-row'>
+            <p>Annually</p>
+            <label className='price-switch'>  {/* it important to have label not div*/}
+              <input 
+                className='price-checkbox'
+                type='checkbox'
+                onChange={() => {
+                  setSelectMonthly((prev) => !prev);
+                }}
+              />
+              <div className='switch-slider'></div>
+            </label>
+            <p>Monthly</p>
+          </div>
+        </header>
+
+        <div className='pricing-cards'>
+          <PricingCard
+            title="Essential"
+            price={selectMonthly ? "20.99" : "188.9"}
+            storage="60 GB Storage"
+            users="5"
+            sendUp="5"
+          />
+          <PricingCard
+            title="Deluxe"
+            price={selectMonthly ? "34.99" : "349.9"}
+            storage="70 GB Storage"
+            users="10"
+            sendUp="10"
+          />
+          <PricingCard
+            title="Premium"
+            price={selectMonthly ? "79.99" : "499.9"}
+            storage="90 GB Storage"
+            users="20"
+          />
+        </div>
+
+
+
+      </div>
     </div>
   );
 }
